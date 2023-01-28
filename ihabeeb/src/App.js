bb75d95e38e645249adba200ed7af8ced4104adb
Habeeb Rahman <habeeb1577@gmail.com>
2023-01-28 14:48:27 +0400

    updated

---
+++ ihabeeb/src/App.js
@@ -0,0 +1,14 @@
+import './main.scss';
+
+function App() {
+  return (
+    <div className="App">
+     <div styleClass="container">
+      <h1>I am Habeeb Rahman </h1>  
+      <p>Product Designer & Developer</p>
+     </div>
+    </div>
+  );
+}
+
+export default App;
