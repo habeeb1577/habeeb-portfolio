2164b5daac19ffbde2e0934adddd65e2b63e5122
Habeeb Rahman <habeeb1577@gmail.com>
2023-01-28 12:49:14 +0400

    updated

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I am Habeeb Rahman</h1>
+      <h3>Product designer & Developer</h3>
+    </div>
+  );
+}
+
+export default App;
